f15cedd05c1460ff8dfa8178403536b0ccdd18e0
Enrico Donelli <enrico.donelli@gmail.com>
2018-04-10 14:47:49 +0200

    barra navigazione in basso

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -36,35 +36,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
       body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new NextEvent(),
@@ -83,6 +60,24 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: new Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar:  new BottomNavigationBar(
+          currentIndex: 0,
+          onTap: (int index) { 
+            print("hai premuto " + index.toString());
+              //setState((){  }); 
+            },
+          items: <BottomNavigationBarItem>[
+            new BottomNavigationBarItem(
+              icon: new Icon(Icons.home),
+              title: new Text("Left"),
+            ),
+            
+            new BottomNavigationBarItem(
+              icon: new Icon(Icons.search),
+              title: new Text("Right"),
+            ),
+        ],
+      ),
     );
   }
 }
--- lib/ui/next_event.dart
+++ lib/ui/next_event.dart
@@ -7,7 +7,7 @@ class NextEvent extends StatefulWidget {
 
 
 class NextEventState extends State<NextEvent> {
-  
+
   @override
   void initState() {
     super.initState();
@@ -16,11 +16,11 @@ class NextEventState extends State<NextEvent> {
   @override
   Widget build(BuildContext context) {
     return new Material(
-      color: Colors.yellowAccent,
+      color: Colors.amber,
       child:  new Padding(
         padding: new EdgeInsets.all(10.0),
         child: new Center(
-          child: new Text("Next event in ",
+          child: new Text("Next event in 6 hours",
           style: new TextStyle(
             fontSize: 20.0,
             fontWeight: FontWeight.bold
